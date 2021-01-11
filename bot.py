0ee93fcb4ba7a0eeb307ebee79c050b864ad5b7f
Silver Adamson <silver.adamson@nortal.com>
2021-01-11 13:03:12 +0200

    minor changes

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import logging
 
 from configuration import ConfigurationLoader
+
+from counter import Counter
 from news.collector import RssItemCollector
 from storage.check import Check
 from storage.history import save
@@ -8,35 +10,29 @@ from twitter.tweet import Tweet
 from twitter.twitterservice import TwitterService
 
 
-class Counter:
-
-    def __init__(self):
-        self.count = 0
-
-    def increment(self):
-        self.count += 1
-
-
 def run() -> None:
     logging.info('Checking RSS feeds...')
     counter = Counter()
 
-    for key, rss in ConfigurationLoader('config/rss.json').get().items():
+    for key, rss_config in ConfigurationLoader('config/rss.json').get().items():
+        if counter.count >= app_config['max_post_count']:
+            break
         logging.info(f'Feed: {key}')
-        rss_items = (RssItemCollector(rss['url'])
+
+        rss_items = (RssItemCollector(rss_config['url'])
                      .collect()
                      .get_items())
 
         for rss_item in rss_items:
             if counter.count >= app_config['max_post_count']:
-                logging.info(f'Max post count achieved ({counter.count}). Stopping')
+                logging.info(f'Max post count achieved ({counter.count}). Stopping...')
                 break
 
             if not Check.is_posted(rss_item.title):
                 hashtags = app_config['constant_tags'] + rss_item.keywords
 
-                if app_config['use_default_tags'] and rss['default_tags']:
-                    hashtags += rss['default_tags']
+                if app_config['use_default_tags'] and rss_item['default_tags']:
+                    hashtags += rss_item['default_tags']
 
                 tweet = Tweet(
                     rss_item.title,
@@ -59,7 +55,6 @@ def run() -> None:
 
 if __name__ == '__main__':
     logging.basicConfig(
-        filename= 
         level=logging.INFO,
         datefmt='%Y-%m-%d %H:%M:%S',
         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s'
